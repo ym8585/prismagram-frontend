9a5b880f721b3f1a5ff6bee512a0fefd56e7b857
ymlee <joo0171@naver.com>
2019-10-22 19:46:34 +0900

    #4.3

---
+++ src/Apollo/LocalState.js
@@ -0,0 +1,25 @@
+export const defaults = {
+    isLoggedIn : localStorage.getItem("token") !== null ? true : false
+    /* 'isLoggedIn : false' -->항상 false 이면 안된다. So, token이 있는지 확인하고 있으면 true*/
+};
+
+export const resolvers = {
+    Mutation : {
+        logUserIn : (_, {token}, {cache}) => {
+            localStorage.setItem("token", token);
+            cache.writerData({
+                data : {
+                    isLoggedIn : true
+                }
+            });
+            return null;
+        },
+        logUserOut : (_, __, {cache}) => {
+            localStorage.removeItem("token");
+            window.location.reload(); /* */
+        }
+    }
+};
+
+
+/* App이 오프라인 상태에서 발생 */
